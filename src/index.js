f70ca1becb812c2d6380599d0f10b2527c83d92e
Ricardo Melo <ricardomelo.rt@gmail.com>
2024-03-17 23:24:49 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'
+import ReactDOM from 'react-dom'
+import React from 'react'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const tag = <strong>Ola React!!</strong>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const el = document.getElementById('root') // Pode ser criada diretamente no parâmetro
+ReactDOM.render(
+    <div>
+        { tag }
+    </div>, 
+    // el  // Foi substituído pela linha abaixo
+    document.getElementById( 'root')
+)
