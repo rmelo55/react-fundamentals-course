3aa5742095287ca22f95e036ac8b92178d068ba8
Ricardo Melo <ricardomelo.rt@gmail.com>
2024-05-30 08:20:42 -0300

    update

--- src/data/products.js
+++ src/data/products.js
@@ -5,6 +5,28 @@ export default [
         price: 300.00
     },
     {
-        id: 2, name: 'Mouse Gamer', price: 20.00
+        id: 2, 
+        name: 'Mouse Gamer', 
+        price: 20.00
+    },
+    {
+        id: 4,
+        name: 'Ipad 9 64Gb',
+        price: 8.00
+    },
+    {
+        id: 5,
+        name: 'Apple Magic Keyboard',
+        price: 200.00
+    },
+    {
+        id: 6,
+        name: 'Apple Pencil 1',
+        price: 150.00
+    },
+    {
+        id: 7,
+        name: 'Apple Magic Mouse',
+        price: 8.00
     }
 ]
